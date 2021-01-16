1f88ccba3d95711f7cff45fc296e749cd34fe714
Rebecca Swernofsky <rswernofsky@gmail.com>
2021-01-16 14:53:52 -0500

    add file uploading functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Tab, Tabs } from 'react-bootstrap'
 import ImageUploadForm from "./components/ImageUploadForm/ImageUploadForm";
 
 const recognize = "recognize";
@@ -14,15 +14,47 @@ const App = () => {
                 <h1>Friend or Foe</h1>
             </header>
             <main>
-                <Container>
+                <Container fluid>
                     <Row className="justify-content-md-center">
-                        <Col md="6">
-                            <h1>Test person form</h1>
-                            <ImageUploadForm endpoint={recognize}/>
-                        </Col>
-                        <Col md="6">
-                            <h1>Add person form</h1>
-                            <ImageUploadForm endpoint={enroll}/>
+                        {/*<Col md="5">*/}
+                        {/*    <h1>Test person form</h1>*/}
+                        {/*    <Tabs defaultActiveKey="upload" id="uncontrolled-tab-example">*/}
+                        {/*        <Tab eventKey="upload" title="Upload">*/}
+                        {/*            <ImageUploadForm*/}
+                        {/*                label="Select Image to Test"*/}
+                        {/*                type="file"*/}
+                        {/*                endpoint={recognize}*/}
+                        {/*            />*/}
+                        {/*        </Tab>*/}
+                        {/*        <Tab eventKey="url" title="URL">*/}
+                        {/*            <ImageUploadForm*/}
+                        {/*                label="Enter Image URL to Test"*/}
+                        {/*                type="text"*/}
+                        {/*                placeholder="Image address URL"*/}
+                        {/*                endpoint={recognize}*/}
+                        {/*            />*/}
+                        {/*        </Tab>*/}
+                        {/*    </Tabs>*/}
+                        {/*</Col>*/}
+                        <Col md="5">
+                            {/*<h2>Add Person</h2>*/}
+                            <Tabs defaultActiveKey="uploadAdd" id="uncontrolled-tab-example">
+                                <Tab eventKey="uploadAdd" title="Upload">
+                                    <ImageUploadForm
+                                        label="Select Image to Add"
+                                        type="file"
+                                        endpoint={enroll}
+                                    />
+                                </Tab>
+                                <Tab eventKey="urlAdd" title="URL">
+                                    <ImageUploadForm
+                                        label="Enter Image URL to Add"
+                                        type="text"
+                                        placeholder="Image address URL"
+                                        endpoint={enroll}
+                                    />
+                                </Tab>
+                            </Tabs>
                         </Col>
                     </Row>
                 </Container>
--- client/src/components/ImageUploadForm/ImageUploadForm.js
+++ client/src/components/ImageUploadForm/ImageUploadForm.js
@@ -1,16 +1,38 @@
-import React, { useState } from 'react'
-import { Form, FormControl } from 'react-bootstrap'
+import React, { useState, useCallback } from 'react'
+import { Form, FormControl, Button } from 'react-bootstrap'
 import classes from './ImageUploadForm.module.css'
 
 const ImageUploadForm = (props) => {
     let [galleryName, setGalleryName] = useState('friend')
     let [name, setName] = useState("");
+    let [file, setFile] = useState(null);
+    let [fileSrc, setFileSrc] = useState('');
+
+    const fileStaging = useCallback((e) => {
+        if (props.type === 'file') {
+            setFile(e.target.files[0])
+
+            let reader = new FileReader();
+
+            try {
+                reader.readAsDataURL(e.target.files[0])
+            } catch (e) {
+                setFileSrc('')
+            }
+
+            reader.addEventListener("load", function() {
+                setFileSrc(reader.result)
+            }, false)
+        } else {
+            setFileSrc(e.target.value)
+        }
+    }, [props.type]);
 
     return (
         <Form className="my-4">
             <Form.Row className="text-left">
                 <Form.Group>
-                    <Form.Label>Gallery</Form.Label>
+                    <Form.Label>{props.label}</Form.Label>
                     <Form.Text className="text-muted">
                         Select upload gallery.
                     </Form.Text>
@@ -42,8 +64,35 @@ const ImageUploadForm = (props) => {
                 </Form.Group>
             </Form.Row>)}
             <Form.Row>
-                select or input image
+                <div className="input-group d-flex justify-content-center mb-3">
+                    <div className="input-group-prepend">
+                        <Button type="submit" variant="primary">
+                            Upload
+                        </Button>
+                    </div>
+                    <div className={props.type === 'file' ? "custom-file" : ''}>
+                        <label
+                            className={props.type === 'file' ? "custom-file-label text-left" : 'text-center'}>
+                            {!file ? props.label : file.name}
+                        </label>
+                        <input
+                            required
+                            type={props.type}
+                            className={props.type === 'file' ? "custom-file-input" : "form-control"}
+                            placeholder={props.placeholder}
+                            onChange={(e) => fileStaging(e)}
+                        />
+                    </div>
+                </div>
             </Form.Row>
+            <h2>Image Preview</h2>
+            {fileSrc ?
+                <figure>
+                    <img class='Image'
+                         alt="Your image"
+                         src={fileSrc} />
+                </figure>
+                : <p style={{ color: "#CCC" }}>No image to preview</p>}
         </Form>
     );
 }
